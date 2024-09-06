c6e9ceb88ceb6325e8e5cc1fc70df3e7403fdc92
Misha <vechnuignor@mail.ru>
2024-09-06 21:31:00 +0300

    addQrCode

--- challenge-2/scripts/onchain.ts
+++ challenge-2/scripts/onchain.ts
@@ -1,6 +1,8 @@
 import { Cell, beginCell, contractAddress, toNano} from "ton-core";
 import { hex } from "../build/main.compiled.json";
 import { TonClient } from "ton";
+import qs from "qs";
+import qrcode from "qrcode-terminal";
 
 async function onchainScript() {
 	const codeCell = Cell.fromBoc(Buffer.from(hex,"hex"))[0];
@@ -12,6 +14,24 @@ async function onchainScript() {
 	});
 
 	console.log("Address: ",address)
+
+	let transactionLink =
+	'https://app.tonkeeper.com/transfer/' +
+	address.toString({
+		testOnly: true,
+	}) +
+	"?" +
+	qs.stringify({
+		text: "Sent simple in",
+		amount: toNano("0.6").toString(10),
+	});
+
+	console.log("Transaction link:",transactionLink);
+
+
+	qrcode.generate(transactionLink, {small: true }, (qr) => {
+		console.log(qr);
+	});
 
 }
 
